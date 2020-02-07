07ea702cb40fa542500929581ce2aecf1f8fa2d1
Marshall Munsch-Hayhurst <marshallmhayhurst@gmail.com>
2020-02-06 19:27:17 -0600

    [Seed] localhot not ready

--- flask-backend/main.py
+++ flask-backend/main.py
@@ -3,4 +3,6 @@ import flask
 app = flask.Flask("__main__")
 
 def my_index():
-  return flask.render_template("index.html")
+  return flask.render_template("index.html", token="Hello Flask+React")
+
+app.run(debug=True)
