22bc2dc57cd86eaffab8da425767175f08970d57
Yulissa Montes <yulissa.montes@utexas.edu>
2020-02-12 18:54:42 -0600

    Added bootstrap and added a button example

---
+++ flask-backend/static/react-new/precache-manifest.6565a9ddafefd0b77718e5d9d4b4a8a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6a971a8711ad9bd2c229db5781acdeb",
+    "url": "/static/react-new/index.html"
+  },
+  {
+    "revision": "bc4a56e1c8bbb308f2d7",
+    "url": "/static/react-new/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "6d48959667c9b9a6fa6c",
+    "url": "/static/react-new/static/js/2.e5a2f960.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/react-new/static/js/2.e5a2f960.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc4a56e1c8bbb308f2d7",
+    "url": "/static/react-new/static/js/main.0fcfdd60.chunk.js"
+  },
+  {
+    "revision": "5f46fe5b854adf02e652",
+    "url": "/static/react-new/static/js/runtime-main.80e57ef5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/react-new/static/media/logo.5d5d9eef.svg"
+  }
+]);
