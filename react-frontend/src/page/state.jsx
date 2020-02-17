85130a8ad3bc0f3349f927d08ce7769f9bd9136c
Yulissa Montes <yulissa.montes@utexas.edu>
2020-02-16 19:41:25 -0600

    created cities listing and css, modified routing for individual states pages

---
+++ react-frontend/src/page/state.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const State = props => {
+  return (
+    <>
+      <h1>State</h1>
+    </>
+  )
+}
+
+State.propTypes = {
+
+}
+
+export default State
