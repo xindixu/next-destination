ba554ac063d0c98006285719a6f42d2c5272b52f
Marshall Munsch-Hayhurst <marshallmhayhurst@gmail.com>
2020-02-17 20:06:43 -0600

    Fixed styling in cards. Now display in a row of 3 and a row of 2

--- react-frontend/src/page/about.jsx
+++ react-frontend/src/page/about.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
-import { Card, CardBody, CardTitle, CardText } from "reactstrap";
+import { Card, CardBody, CardTitle, CardText, Container, Row, Col } from "reactstrap";
 import apiFetch from "../lib/api-fetch";
 
 const People = props => {
@@ -15,25 +15,36 @@ const People = props => {
   }, []);
 
   return (
-    <div>
+    <div className="body">
+			<h1>About Us</h1>
+			<h2>The Project</h2>
+
+
+    <Container>
+    <h2>The Team</h2>
+    <Row>
       {people.length ? (
         people.map(
           ({ name, photo, stats: { commits, issues }, description }) => (
+            <Col xs={12} sm={6} md={4} lg={4}>
             <Card key={name}>
-              <CardTitle>{name}</CardTitle>
               <CardBody>
+                <CardTitle>{name}</CardTitle>
                 <CardText>{description}</CardText>
-                <br />
                 <CardText> Commits: {commits} </CardText>
                 <CardText> Issues: {issues} </CardText>
               </CardBody>
             </Card>
+            </Col>
           )
         )
       ) : (
         <p>Fetching data...</p>
       )}
+    </Row>
+    </Container>
     </div>
+
   );
 };
 People.propTypes = {};
