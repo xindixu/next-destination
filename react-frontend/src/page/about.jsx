6ccda0296d1107148f1517405972e5f30a2ec7b3
Yulissa Montes <yulissa.montes@utexas.edu>
2020-02-18 20:12:12 -0600

    removed ====HEAD from about.jsx and extra imports

--- react-frontend/src/page/about.jsx
+++ react-frontend/src/page/about.jsx
@@ -1,16 +1,10 @@
-<<<<<<< HEAD
 import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
 import { Card, CardBody, CardTitle, CardText, CardImg, CardDeck } from 'reactstrap'
-import { Container, Row, Col, Spinner} from 'reactstrap'
+import { Container, Row, Col, Spinner } from 'reactstrap'
 import apiFetch from '../lib/api-fetch'
 import yoda from '../assets/yoda-portrait.jpg'
 import './about.css'
-=======
-import React, { useEffect, useState } from "react";
-import PropTypes from "prop-types";
-import { Card, CardBody, CardTitle, CardText } from "reactstrap";
-import apiFetch from "../lib/api-fetch";
 
 const People = props => {
   const [people, setPeople] = useState([]);
@@ -26,32 +20,32 @@ const People = props => {
   return (
 
     <div className="body">
-			<h1>About Us</h1>
-			<h2>The Project</h2>
+      <h1>About Us</h1>
+      <h2>The Project</h2>
 
-    <Container className="themed-container">
+      <Container className="themed-container">
         <h2>The Team</h2>
-     <Row>
-     <Col md='4'>
-      {people.length ? people.map(
-        ({ name, photo, stats: { commits, issues }, description }) =>
-        <Row className='no-gutters'>
-            <Col md='3'>
-                <Card>
-                <CardBody>
-                    <CardTitle>{name}</CardTitle>
-                    <CardText>{description}</CardText>
-                    <CardText> Commits: {commits} </CardText>
-                    <CardText> Issues: {issues} </CardText>
-                </CardBody>
-                </Card>
-             </Col>
-         </Row>
+        <Row>
+          <Col md='4'>
+            {people.length ? people.map(
+              ({ name, photo, stats: { commits, issues }, description }) =>
+                <Row className='no-gutters'>
+                  <Col md='3'>
+                    <Card>
+                      <CardBody>
+                        <CardTitle>{name}</CardTitle>
+                        <CardText>{description}</CardText>
+                        <CardText> Commits: {commits} </CardText>
+                        <CardText> Issues: {issues} </CardText>
+                      </CardBody>
+                    </Card>
+                  </Col>
+                </Row>
 
-      ) : <Spinner type="grow" color="info" />}
-       </Col>
-      </Row>
-    </Container>
+            ) : <Spinner type="grow" color="info" />}
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
