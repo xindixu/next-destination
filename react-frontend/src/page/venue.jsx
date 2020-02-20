125fc50a7ef26b808f49c82ed7d26a05ccb57b7d
Xindi Xu <xindixu@utexas.edu>
2020-02-20 17:36:07 -0600

    sorting in venues

--- react-frontend/src/page/venue.jsx
+++ react-frontend/src/page/venue.jsx
@@ -1,22 +1,21 @@
-import React, { useEffect, useState } from 'react'
-import PropTypes from 'prop-types'
+import React, { useEffect, useState } from "react";
+import PropTypes from "prop-types";
 import { useParams, Link } from "react-router-dom";
-import apiFetch from '../lib/api-fetch'
-import './city.css'
+import { filterCities, filterArtists } from "../lib/util";
+import apiFetch from "../lib/api-fetch";
+import "./city.css";
 
 const Venue = () => {
   const { id } = useParams();
 
-  const [venue, setVenue] = useState(null)
+  const [venue, setVenue] = useState(null);
   const [cities, setCities] = useState([]);
   const [artists, setArtists] = useState([]);
   useEffect(() => {
     apiFetch(`/venue/${id}`, {})
       .then(resp => resp.json())
-      .then(data => {
-        setVenue(data.venue)
-      })
-  }, [])
+      .then(data => setVenue(data.venue));
+  }, []);
 
   useEffect(() => {
     apiFetch("/cities", {})
@@ -34,50 +33,36 @@ const Venue = () => {
       });
   }, []);
 
-  const filterCities = (venueCity, cities) => {
-    const city = cities.filter(cities => cities.name === venueCity)
-    console.log(city)
-    return city
-  }
-
-  const filterArtists = (venueArtist, artists) => {
-    const artist = artists.filter(artists => artists.name === venueArtist)
-    return artist
-  }
-
   if (venue) {
-    const { name, coordinates, city, capacity, artist, pic } = venue
+    const { name, coordinates, city, capacity, artist, pic } = venue;
     return (
       <>
         <div className="city1">
           <h1> {name} </h1>
           <img src={`${pic}`} alt="Pic of Venue" />
 
-
           <div>
-            {filterCities(city, cities).map(cities => (
-              <p> City: <Link to={`/city/${cities.id}`}>{cities.name}</Link> </p>
-
+            {filterCities(city, cities).map(({ id, name }) => (
+              <p>
+                City: <Link to={`/city/${id}`}>{name}</Link>
+              </p>
             ))}
-            {filterArtists(artist, artists).map(artists => (
-              <p> Upcoming Artists: <Link to={`/artist/${artists.id}`}>{artists.name}</Link> </p>
-
+            {filterArtists(artist, artists).map(({ id, name }) => (
+              <p>
+                Upcoming Artists: <Link to={`/artist/${id}`}>{name}</Link>
+              </p>
             ))}
             <p> capacity: {capacity}</p>
             <p> coordinates: {coordinates} </p>
           </div>
-
         </div>
-
       </>
-    )
+    );
   }
 
-  return <> </>
-}
+  return <> </>;
+};
 
-Venue.propTypes = {
+Venue.propTypes = {};
 
-}
-
-export default Venue
+export default Venue;
--- react-frontend/src/page/venues.jsx
+++ react-frontend/src/page/venues.jsx
@@ -1,23 +1,20 @@
-import React, { useEffect, useState } from 'react'
-import PropTypes from 'prop-types'
-import apiFetch from '../lib/api-fetch'
-import './cities.css'
-import { Table } from "reactstrap";
-import SortableTable from '../components/sortable-table'
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import apiFetch from "../lib/api-fetch";
+import "./cities.css";
+import SortableTable from "../components/sortable-table";
+import { filterCities, filterArtists } from "../lib/util";
 
 const Venues = props => {
-  const [venues, setVenues] = useState([])
+  const [venues, setVenues] = useState([]);
   const [cities, setCities] = useState([]);
   const [artists, setArtists] = useState([]);
   useEffect(() => {
-    apiFetch('/venues', {})
+    apiFetch("/venues", {})
       .then(resp => resp.json())
-      .then(data => {
-        setVenues(data.venues)
-        console.log(data.venues)
-      })
-  }, [])
+      .then(data => setVenues(data.venues));
+  }, []);
 
   useEffect(() => {
     apiFetch("/cities", {})
@@ -35,47 +32,46 @@ const Venues = props => {
       });
   }, []);
 
-  const filterCities = (venueCity) => {
-    const city = cities.filter(cities => cities.name === venueCity)
-    console.log(city)
-    return city
-  }
-
-  const filterArtists = (venueArtist) => {
-    const artist = artists.filter(artist => artist.name === venueArtist)
-    return artist
-  }
-
   const venueCitiesComponent = ({ city }) => {
-    const cities = filterCities(city)
+    const venueCities = filterCities(city, cities);
 
-    return (<span>{
-      cities.map(city => (
-        <Link to={`/city/${city.id}`}>{city.name}, {city.state}</Link>
-      ))}
-    </span>)
-  }
+    return (
+      <span>
+        {venueCities.map(({ id, name, state }) => (
+          <Link to={`/city/${id}`}>
+            {name}, {state}
+          </Link>
+        ))}
+      </span>
+    );
+  };
 
   const venueArtistsComponent = ({ artist }) => {
-    const artists = filterArtists(artist)
+    const venueArtists = filterArtists(artist, artists);
 
-    return (<span>{
-      artists.map(artist => (
-        <Link to={`/artist/${artist.id}`}>{artist.name}</Link>
-      ))}
-    </span>)
-  }
+    return (
+      <span>
+        {venueArtists.map(({ id, name }) => (
+          <Link to={`/artist/${id}`}>{name}</Link>
+        ))}
+      </span>
+    );
+  };
 
   const settings = {
     pic: {
       title: "Picture",
-      getBodyFormat: (_, { pic, name }) => <img src={pic} alt={`Picture for venue ${name}`} />,
+      getBodyFormat: (_, { pic, name }) => (
+        <img src={pic} alt={`Venue ${name}`} />
+      ),
       isKey: false,
       dataSort: false
     },
     name: {
       title: "Name",
-      getBodyFormat: (_, { id, name }) => <Link to={`/venue/${id}`}>{name}</Link>,
+      getBodyFormat: (_, { id, name }) => (
+        <Link to={`/venue/${id}`}>{name}</Link>
+      ),
       isKey: true,
       dataSort: true
     },
@@ -83,50 +79,45 @@ const Venues = props => {
       title: "City",
       getBodyFormat: (_, object) => venueCitiesComponent(object),
       isKey: false,
-      dataSort: true,
+      dataSort: true
     },
     artist: {
       title: "Upcoming Artists",
       getBodyFormat: (_, object) => venueArtistsComponent(object),
       isKey: false,
-      dataSort: true,
+      dataSort: true
     },
     capacity: {
       title: "Capacity",
-      getBodyFormat: (_, { capacity }) => <span>{capacity}</span>,
+      getBodyFormat: (_, { capacity }) => capacity,
       isKey: false,
-      dataSort: true,
+      dataSort: true
     },
     coordinates: {
       title: "Coordinates",
-      getBodyFormat: (_, {coordinates}) => coordinates,
+      getBodyFormat: (_, { coordinates }) => coordinates,
       isKey: false,
-      dataSort: true,
+      dataSort: true
     },
     restaurant: {
       title: "Restaurants Close By",
-      getBodyFormat: (_, {  restaurant}) => restaurant,
+      getBodyFormat: (_, { restaurant }) => restaurant,
       isKey: false,
-      dataSort: false
+      dataSort: true
     }
-  }
+  };
 
   if (venues.length && cities.length) {
     return (
       <>
         <h1>Venues</h1>
-        <SortableTable
-          data={venues}
-          settings={settings}
-        />
+        <SortableTable data={venues} settings={settings} />
       </>
     );
   }
 
-  return <></>
-}
-Venues.propTypes = {
+  return <></>;
+};
+Venues.propTypes = {};
 
-}
-
-export default Venues
+export default Venues;
