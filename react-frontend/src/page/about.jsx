0e4ad293c51c836cf6dd02eeb0207e461962694e
Xindi Xu <xindixu@utexas.edu>
2020-02-20 17:39:59 -0600

    linter warnings

--- react-frontend/src/page/about.jsx
+++ react-frontend/src/page/about.jsx
@@ -1,22 +1,31 @@
-import React, { useEffect, useState } from 'react'
-import PropTypes from 'prop-types'
-import { Card, CardBody, CardTitle, CardText, CardImg, CardDeck } from 'reactstrap'
-import { Container, Row, Col, Spinner } from 'reactstrap'
-import apiFetch from '../lib/api-fetch'
-import marshall_pic from '../assets/marshall_portrait.jpg'
-import yulissa_pic from '../assets/yulissa_portrait.jpg'
-import quinton_pic from '../assets/quinton_portrait.jpg'
-import nathan_pic from '../assets/nathan_portrait.jpg'
-import xindi_pic from '../assets/xindi_portrait.jpg'
-import './about.css'
+import React, { useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import {
+  Card,
+  CardBody,
+  CardTitle,
+  CardText,
+  Container,
+  Row,
+  Col,
+  Spinner
+} from "reactstrap";
+
+import apiFetch from "../lib/api-fetch";
+import marshallPhoto from "../assets/marshall_portrait.jpg";
+import yulissaPhoto from "../assets/yulissa_portrait.jpg";
+import quintonPhoto from "../assets/quinton_portrait.jpg";
+import nathanPhoto from "../assets/nathan_portrait.jpg";
+import xindiPhoto from "../assets/xindi_portrait.jpg";
+import "./about.css";
 
 const photos = {
-   marshall: marshall_pic,
-   yulissa: yulissa_pic,
-   quinton: quinton_pic,
-   nathan: nathan_pic,
-   xindi: xindi_pic
-}
+  marshall: marshallPhoto,
+  yulissa: yulissaPhoto,
+  quinton: quintonPhoto,
+  nathan: nathanPhoto,
+  xindi: xindiPhoto
+};
 const People = props => {
   const [people, setPeople] = useState([]);
   useEffect(() => {
@@ -29,64 +38,186 @@ const People = props => {
   }, []);
 
   return (
-
     <div className="body">
-
-	<Container>
-    <h1>About Us</h1>
-	<h2>The Project</h2>
-    <div className='flex-links'>
-    <p>This project connects music-focused travellers to artists, venues, and cities.</p>
-    <ul>
-        <p><b>Links about our project</b></p>
-        <li><a href="https://gitlab.com/nmcraig/cs-331e/issues">Our Issue Tracker</a></li>
-        <li><a href="https://gitlab.com/nmcraig/cs-331e/">Our Repo</a></li>
-        <li><a href="https://gitlab.com/nmcraig/cs-331e/-/wikis/Phase-1-Development">Our report for phase 1 Development</a></li>
-        <li><a href="https://gitlab.com/nmcraig/cs-331e/-/wikis/Technical-Report">The overall technical report</a></li>
-        <p><b>Links about Data</b></p>
-        <li><a href="https://www.artists.bandsintown.com/support/api-installation">Artists and Venues through BandsinTown</a></li>
-        <li><a href="http://developers.teleport.org/api/getting_started/#search_name">City Searching Feature and Photo Database</a></li>
-        <li><a href="https://www.mediawiki.org/wiki/API:Main_page">Information about Cities</a></li>
-        <li><a href="http://insideairbnb.com/get-the-data.html">Airbnb Data for Different Cities</a></li>
-        <p><b>Links about Tools</b></p>
-        <li><a href="https://reactjs.org/">Frontend- Reactjs</a></li>
-        <li><a href="https://palletsprojects.com/p/flask/">Backend- Flask</a></li>
-        <li><a href="https://reactstrap.github.io/">CSS- Reactstrap- a bootstrap library for react</a></li>
-        <li><a href="http://insideairbnb.com/get-the-data.html">Airbnb Data for Different Cities</a></li>
-        <li><a href="https://slack.com/">Communication and gitlab integration done with Slack</a></li>
-        <li><a href="https://cloud.google.com/">Deployment through Google Cloud Platform</a></li>
-    </ul>
-    </div>
-    <h2>The Team</h2>
-    <Row>
-
-      {people.length ? (
-        people.map(
-          ({ name, responsibilities, stats: { commits, issues }, description, id }) => (
-            <Col xs={12} sm={6} md={4} lg={4}>
-            <Card key={name}>
-              <CardBody>
-                <CardTitle>{name}</CardTitle>
-                <img src={photos[id]} ></img>
-                <CardText>Skills: {responsibilities}</CardText>
-                <CardText>Description: {description}</CardText>
-                <CardText> Commits: {commits} </CardText>
-                <CardText> Issues: {issues} </CardText>
-              </CardBody>
-            </Card>
-            </Col>
-          )
-        )
-      ) : (
-        <div className='abs_spinner'>
-            <Spinner type="grow" color="info" />
+      <Container>
+        <h1>About Us</h1>
+        <h2>The Project</h2>
+        <div className="flex-links">
+          <p>
+            This project connects music-focused travellers to artists, venues,
+            and cities.
+          </p>
+          <ul>
+            <p>
+              <b>Links about our project</b>
+            </p>
+            <li>
+              <a
+                href="https://gitlab.com/nmcraig/cs-331e/issues"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Our Issue Tracker
+              </a>
+            </li>
+            <li>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://gitlab.com/nmcraig/cs-331e/"
+              >
+                Our Repo
+              </a>
+            </li>
+            <li>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://gitlab.com/nmcraig/cs-331e/-/wikis/Phase-1-Development"
+              >
+                Our report for phase 1 Development
+              </a>
+            </li>
+            <li>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://gitlab.com/nmcraig/cs-331e/-/wikis/Technical-Report"
+              >
+                The overall technical report
+              </a>
+            </li>
+            <p>
+              <b>Links about Data</b>
+            </p>
+            <li>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.artists.bandsintown.com/support/api-installation"
+              >
+                Artists and Venues through BandsinTown
+              </a>
+            </li>
+            <li>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="http://developers.teleport.org/api/getting_started/#search_name"
+              >
+                City Searching Feature and Photo Database
+              </a>
+            </li>
+            <li>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.mediawiki.org/wiki/API:Main_page"
+              >
+                Information about Cities
+              </a>
+            </li>
+            <li>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="http://insideairbnb.com/get-the-data.html"
+              >
+                Airbnb Data for Different Cities
+              </a>
+            </li>
+            <p>
+              <b>Links about Tools</b>
+            </p>
+            <li>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://reactjs.org/"
+              >
+                Frontend- Reactjs
+              </a>
+            </li>
+            <li>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://palletsprojects.com/p/flask/"
+              >
+                Backend- Flask
+              </a>
+            </li>
+            <li>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://reactstrap.github.io/"
+              >
+                CSS- Reactstrap- a bootstrap library for react
+              </a>
+            </li>
+            <li>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="http://insideairbnb.com/get-the-data.html"
+              >
+                Airbnb Data for Different Cities
+              </a>
+            </li>
+            <li>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://slack.com/"
+              >
+                Communication and gitlab integration done with Slack
+              </a>
+            </li>
+            <li>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://cloud.google.com/"
+              >
+                Deployment through Google Cloud Platform
+              </a>
+            </li>
+          </ul>
         </div>
-      )}
-    </Row>
-    </Container>
-
+        <h2>The Team</h2>
+        <Row>
+          {people.length ? (
+            people.map(
+              ({
+                name,
+                responsibilities,
+                stats: { commits, issues },
+                description,
+                id
+              }) => (
+                <Col xs={12} sm={6} md={4} lg={4}>
+                  <Card key={name}>
+                    <CardBody>
+                      <CardTitle>{name}</CardTitle>
+                      <img src={photos[id]} alt={name} />
+                      <CardText>Skills: {responsibilities}</CardText>
+                      <CardText>Description: {description}</CardText>
+                      <CardText> Commits: {commits} </CardText>
+                      <CardText> Issues: {issues} </CardText>
+                    </CardBody>
+                  </Card>
+                </Col>
+              )
+            )
+          ) : (
+            <div className="abs_spinner">
+              <Spinner type="grow" color="info" />
+            </div>
+          )}
+        </Row>
+      </Container>
     </div>
-
   );
 };
 People.propTypes = {};
