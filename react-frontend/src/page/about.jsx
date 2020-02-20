0757c2efa896a324c29b2f7731a38753105419a7
Marshall Munsch-Hayhurst <marshallmhayhurst@gmail.com>
2020-02-19 20:04:14 -0600

    About photos integrating

--- react-frontend/src/page/about.jsx
+++ react-frontend/src/page/about.jsx
@@ -3,10 +3,20 @@ import PropTypes from 'prop-types'
 import { Card, CardBody, CardTitle, CardText, CardImg, CardDeck } from 'reactstrap'
 import { Container, Row, Col, Spinner } from 'reactstrap'
 import apiFetch from '../lib/api-fetch'
-import yoda from '../assets/yoda-portrait.jpg'
-
+import marshall_pic from '../assets/marshall_portrait.jpg'
+import yulissa_pic from '../assets/yulissa_portrait.jpg'
+import quinton_pic from '../assets/quinton_portrait.jpg'
+import nathan_pic from '../assets/nathan_portrait.jpg'
+import xindi_pic from '../assets/xindi_portrait.jpg'
 import './about.css'
 
+const photos = {
+   marshall: marshall_pic,
+   yulissa: yulissa_pic,
+   quinton: quinton_pic,
+   nathan: nathan_pic,
+   xindi: xindi_pic
+}
 const People = props => {
   const [people, setPeople] = useState([]);
   useEffect(() => {
@@ -21,20 +31,22 @@ const People = props => {
   return (
 
     <div className="body">
-			<h1>About Us</h1>
-			<h2>The Project</h2>
+
+	<Container>
+	<h1>About Us</h1>
+	<h2>The Project</h2>
 
 
-    <Container>
     <h2>The Team</h2>
     <Row>
       {people.length ? (
         people.map(
-          ({ name, photo, responsibilities, stats: { commits, issues }, description }) => (
+          ({ name, responsibilities, stats: { commits, issues }, description, id }) => (
             <Col xs={12} sm={6} md={4} lg={4}>
             <Card key={name}>
               <CardBody>
                 <CardTitle>{name}</CardTitle>
+                <img src={photos[id]} ></img>
                 <CardText>Skills: {responsibilities}</CardText>
                 <CardText>Description: {description}</CardText>
                 <CardText> Commits: {commits} </CardText>
