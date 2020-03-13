2737a919a64e4bef463f2087f9c7d7f01b3e36ad
Yulissa Montes <yulissa.montes@utexas.edu>
2020-03-13 17:58:34 -0500

    Adding airbnb data to database

--- flask-backend/models.py
+++ flask-backend/models.py
@@ -20,33 +20,34 @@ class Cities(db.Model):
     population = db.Column(db.Integer, nullable=False)
     description = db.Column(db.String(5000), nullable=False)
 
+    listings = db.relationship('Airbnb', backref='cities')
 
-# class Airbnb(db.Model):
-#     __table__name = 'airbnb'
 
-#     id = db.Column(db.Float, primary_key=True)
-#     listing_url = db.Column(db.String(80), nullable=False)
-#     name = db.Column(db.String(80), nullable=False)
-#     description = db.Column(db.String(400), nullable=False)
-#     neighborhood_overview = db.Column(db.String(400), nullable=True)
-#     transit = db.Column(db.String(400), nullable=False)
-#     picture_url = db.Column(db.String(400), nullable=False)
-#     neighbourhood = db.Column(db.String(400), nullable=True)
-#     city = db.Column(db.String(50), nullable=False)
-#     state = db.Column(db.String(50), nullable=False)
-#     zipcode = db.Column(db.Integer, nullable=True)
-#     smart_location = db.Column(db.String(80), nullable=False)
-#     latitude = db.Column(db.Float, nullable=False)
-#     longitude = db.Column(db.Float, nullable=False)
-#     room_type = db.Column(db.String(80), nullable=False)
-#     accomodates = db.Column(db.Float, nullable=False)
-#     amenities = db.Column(db.String(400), nullable=False)
-#     price = db.Column(db.String(20), nullable=False)
-#     number_of_reviews = db.Column(db.Integer, nullable=False)
-#     review_scores_rating = db.Column(db.Float, nullable=False)
-#     # attempt no. juan
-#     city_name = db.Column(db.String(40), db.ForeignKey(
-#         'cities.name'), nullable=False)
+class Airbnb(db.Model):
+    __table__name = 'airbnb'
+
+    id = db.Column(db.Float, primary_key=True)
+    listing_url = db.Column(db.String(80), nullable=False)
+    name = db.Column(db.String(80), nullable=False)
+    description = db.Column(db.String(2000), nullable=False)
+    neighborhood_overview = db.Column(db.String(2000), nullable=True)
+    transit = db.Column(db.String(2000), nullable=False)
+    picture_url = db.Column(db.String(400), nullable=False)
+    neighbourhood = db.Column(db.String(1200), nullable=True)
+    city = db.Column(db.String(50), nullable=False)
+    state = db.Column(db.String(50), nullable=False)
+    zipcode = db.Column(db.Float, nullable=True)
+    smart_location = db.Column(db.String(80), nullable=False)
+    latitude = db.Column(db.Float, nullable=False)
+    longitude = db.Column(db.Float, nullable=False)
+    room_type = db.Column(db.String(80), nullable=False)
+    accomodates = db.Column(db.Float, nullable=False)
+    amenities = db.Column(db.String(2000), nullable=False)
+    price = db.Column(db.String(20), nullable=False)
+    number_of_reviews = db.Column(db.Integer, nullable=False)
+    review_scores_rating = db.Column(db.Float, nullable=False)
+    city_name = db.Column(db.String(40), db.ForeignKey(
+        'cities.name'), nullable=False)
 
 
 db.create_all()
