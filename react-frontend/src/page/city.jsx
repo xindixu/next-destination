a2917039bff56c72ea6581d16f09ab5eaeb17df5
Marshall Munsch-Hayhurst <marshallmhayhurst@gmail.com>
2020-03-20 21:18:39 -0500

    Fixed Styling on City.jsx

--- react-frontend/src/page/city.jsx
+++ react-frontend/src/page/city.jsx
@@ -3,7 +3,9 @@ import PropTypes from "prop-types";
 import { useParams } from "react-router-dom";
 import Tabs from "react-bootstrap/Tabs";
 import Tab from "react-bootstrap/Tab";
-
+import Table from "react-bootstrap/Table"
+import Col from "react-bootstrap/Col"
+import Row from "react-bootstrap/Row"
 import apiFetch from "../lib/api-fetch";
 import Restaurants from "../containers/restaurants";
 import Events from "../containers/events";
@@ -28,6 +30,7 @@ const City = () => {
   const { name } = useParams();
 
   const [city, setCity] = useState(null);
+  const [images, setImages] = useState([]);
   const [isError, setIsError] = useState(false);
 
   // TODO: data should be passed down from parent
@@ -41,21 +44,49 @@ const City = () => {
       });
   }, [name]);
 
+  useEffect(() => {
+    fetch(`https://api.teleport.org/api/urban_areas/slug:${name.toLowerCase()}/images/`)
+      .then(resp => resp.json())
+      .then(data => {
+        setImages(data['photos'][0]['image']['web'])
+        console.log(data['photos'][0]['image']['web'])
+        });
+  }, []);
+
   if (city) {
-    const { state, latitude, longitude, population, description } = city;
+    const { state, latitude, longitude, population, description, image } = city;
     return (
       <>
         <div className="city1">
-          <h1> {name} </h1>
-          <p> {description} </p>
-          {/* TODO: extract this component */}
-          <img id="randCity1" alt="pic of city" />
-          <div>
-            <p>state: {state}</p>
-            <p>latitude: {latitude}</p>
-            <p>longitude: {longitude}</p>
-            <p>population: {population}</p>
+          <div className="city-image-container">
+            <img id="img_city" src={`${images}`} alt='pic of city'/>
           </div>
+          <h1> {name}, {state} </h1>
+
+          {/* TODO: extract this component */}
+         <div className="des-sec-container">
+            <h2>Description</h2>
+            <p> {description} </p>
+          </div>
+        </div>
+
+        <div className="stat-container">
+            <h2>Statistics</h2>
+            <div className='stat-table'>
+                <Table>
+                    <tr>
+                        <th>Population</th>
+                        <th>Longitude</th>
+                        <th>Latitude</th>
+                    </tr>
+                    <tr>
+                        <td>{population}</td>
+                        <td>{longitude}</td>
+                        <td>{latitude}</td>
+                    </tr>
+                </Table>
+            </div>
+
         </div>
 
         <Tabs defaultActiveKey={TABS.restaurants.key}>
