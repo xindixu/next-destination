5b00592c1905aded2f0ae3d23ad05612a34614ad
Yulissa Montes <yulissa.montes@utexas.edu>
2020-03-22 13:58:28 -0500

    Added nearby events, restaurants, airbnbs for event.jsx

--- react-frontend/src/page/event.jsx
+++ react-frontend/src/page/event.jsx
@@ -1,7 +1,27 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-
+import Tabs from "react-bootstrap/Tabs";
+import Tab from "react-bootstrap/Tab";
 import apiFetch from "../lib/api-fetch";
+import Events from "../containers/events";
+import Restaurants from "../containers/restaurants";
+import Airbnbs from "../containers/airbnbs";
+import { EVENT_SCHEMA, RESTAURANT_SCHEMA, AIRBNB_SCHEMA } from "../lib/constants";
+
+const TABS = {
+  events: {
+    key: "events",
+    title: "Nearby Events"
+  },
+  restaurants: {
+    key: "restaurants",
+    title: "Nearby Restaurants"
+  },
+  airbnbs: {
+    key: "airbnbs",
+    title: "Nearby Airbnbs"
+  }
+}
 
 const Event = () => {
   const { id } = useParams();
@@ -20,7 +40,8 @@ const Event = () => {
   const {
     name,
     category,
-    location: { display_address: address },
+    categories,
+    location: { display_address: address, city },
     image_url: image,
     latitude,
     longitude,
@@ -49,11 +70,31 @@ const Event = () => {
         ({latitude}, {longitude}){" "}
       </p>
       <p>
-        <a href={url}>Official Website</a>
+        <a href={url}>Event Website</a>
       </p>
       <p>{address}</p>
       <p>{cost}</p>
       <p>{description}</p>
+
+      <Tabs defaultActiveKey={TABS.events.key}>
+        <Tab eventKey={TABS.events.key} title={TABS.events.title}>
+          <Events
+            coordinates={{ longitude, latitude }}
+            tableSchema={EVENT_SCHEMA}
+          />
+        </Tab>
+
+        <Tab eventKey={TABS.restaurants.key} title={TABS.restaurants.title}>
+          <Restaurants
+            initialFilters={{ category: categories }}
+            coordinates={{ longitude, latitude }}
+            tableSchema={RESTAURANT_SCHEMA}
+          />
+        </Tab>
+        <Tab eventKey={TABS.airbnbs.key} title={TABS.airbnbs.title}>
+          <Airbnbs city={city} />
+        </Tab>
+      </Tabs>
     </>
   );
 };
