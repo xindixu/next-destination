a7ee97789d72d34dc331522c704e9a495a1681ba
Nathan Craig <n.craig@gmail.com>
2020-03-30 14:02:58 -0500

    Added comments for pydoc of models.py

--- flask-backend/models.py
+++ flask-backend/models.py
@@ -13,6 +13,14 @@ db = SQLAlchemy(app)
 
 
 class Cities(db.Model):
+    '''
+    The Cities class implements an sql table that holds data on the 100 most populous cities.
+    It establishes 8 columns: name, state, latitude, longitude, population, description, id, and listings.
+    It uses name as the primary key and has id as a secondary means to identify if there are 
+    two cities with the same name.
+    The listings column is connected to the Airbnb table to populate the Airbnb listings located in 
+    that city if the Airbnb table has listings for it.
+    '''
     __tablename__ = 'cities'
 
     name = db.Column(db.String(80), primary_key=True)
@@ -29,6 +37,17 @@ class Cities(db.Model):
 
 
 class Airbnb(db.Model):
+    '''
+    The Airbnb class holds data for Airbnb listings for various cities in the U.S. 
+    There are not listings for every city in the Cities table so data gets sent to 
+    that table only if the Airbnb table has data for it.
+    The Airbnb table has 21 columns: id, listing_url, name, description, neighborhood_overview,
+    transit, picture_url, neighborhood, city, state, zipcode, smart_location, latitude, longitude,
+    room_type, accomodates, amenities, price, number_of_reviews, review_scores_rating, and city_name.
+    id is the primary key for this table and city_name is the foreign key (primary key for the Cities table), 
+    which connects the two tables.
+    '''
+
     __table__name = 'airbnb'
 
     id = db.Column(db.Float, primary_key=True)
