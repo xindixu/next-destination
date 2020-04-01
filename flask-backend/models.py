30a0e9f54deca64d1bec6b4cba008b9d86666b11
Nathan Craig <n.craig@gmail.com>
2020-04-01 10:55:30 -0500

    Pydoc and gitlog complete

--- flask-backend/models.py
+++ flask-backend/models.py
@@ -7,20 +7,20 @@ from sqlalchemy import create_engine
 app = Flask(__name__)
 # TODO: replace with real password
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
-    "DB_STRING", 'postgres://postgres:supersecret@localhost:5432/cityhuntdb')
+    "DB_STRING", 'postgres://postgres:Nmc!2342@localhost:5432/cityhuntdb')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 
 
 class Cities(db.Model):
-    '''
+    """
     The Cities class implements an sql table that holds data on the 100 most populous cities.
     It establishes 8 columns: name, state, latitude, longitude, population, description, id, and listings.
     It uses name as the primary key and has id as a secondary means to identify if there are 
     two cities with the same name.
     The listings column is connected to the Airbnb table to populate the Airbnb listings located in 
     that city if the Airbnb table has listings for it.
-    '''
+    """
     __tablename__ = 'cities'
 
     name = db.Column(db.String(80), primary_key=True)
@@ -37,7 +37,7 @@ class Cities(db.Model):
 
 
 class Airbnb(db.Model):
-    '''
+    """
     The Airbnb class holds data for Airbnb listings for various cities in the U.S. 
     There are not listings for every city in the Cities table so data gets sent to 
     that table only if the Airbnb table has data for it.
@@ -46,7 +46,7 @@ class Airbnb(db.Model):
     room_type, accomodates, amenities, price, number_of_reviews, review_scores_rating, and city_name.
     id is the primary key for this table and city_name is the foreign key (primary key for the Cities table), 
     which connects the two tables.
-    '''
+    """
 
     __table__name = 'airbnb'
 
