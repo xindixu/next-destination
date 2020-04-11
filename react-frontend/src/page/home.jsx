d8769df6a271d6f909fea38b5eb3b2bd7c7efc62
Marshall Munsch-Hayhurst <marshallmhayhurst@gmail.com>
2020-04-11 10:40:00 -0500

    Changed fetch url

--- react-frontend/src/page/home.jsx
+++ react-frontend/src/page/home.jsx
@@ -2,15 +2,17 @@ import React, { useEffect, useState } from "react";
 import { Row, Col, Container } from "react-bootstrap";
 import "./home.css";
 import apiFetch from "../lib/api-fetch";
+import { useParams } from "react-router-dom";
 
 
 const Home = () => {
+  const { id } = useParams();
   const [city, setCity] = useState(null);
   const [image, setImage] = useState("");
   const [isError, setIsError] = useState(false);
   
   useEffect(() => {
-    apiFetch(`/random-city`, {})
+    apiFetch(`/city/random`, {})
       .then(data => {
         console.log("BON")
         setCity(data.city);
@@ -19,8 +21,9 @@ const Home = () => {
         console.log("NON")
         setIsError(true);
       });
-  }, []);
+  }, [id]);
   console.log(city)
+  
   // useEffect(() => {
   //   fetch(`https://api.teleport.org/api/urban_areas/slug:${t_id}/images/`)
   //     .then(resp => resp.json())
@@ -63,7 +66,28 @@ const Home = () => {
     );
   };
   return <>
-    <h1>NO CITY FOUND</h1>
+    <div id="home">
+        <div className="title">
+          <h1>City Hunt</h1>
+            <p className="description">
+              Discover something new!
+              Make your dream vacation happen.. City Hunt is a centralized application designed to simplify travel arrangements.
+              Try our random city generator to start discovering new places!
+            </p>
+        </div>
+        <div className="bottom-text-container">
+          <Container className="Container">
+            <Row>
+              <Col className="text">
+                
+                <p className="city-name">Nowhere</p>
+                <p> Population: 0</p>
+                <p> Description: This city is in the middle of nowhere.</p>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+      </div>
   </>;
 };
 
