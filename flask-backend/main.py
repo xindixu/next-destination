cdc6c9c3298980745b605c3b3c352fcde0b9207f
Marshall Munsch-Hayhurst <marshallmhayhurst@gmail.com>
2020-04-12 21:23:57 -0500

    Fixed API

--- flask-backend/main.py
+++ flask-backend/main.py
@@ -305,13 +305,13 @@ def city_rand():
 
 @app.route('/api/search_cities/<string:term>', methods=['GET', 'POST', "OPTIONS"])
 def search_cities(term):
-    if (term):
         try:
-            query_data = session.query(Cities).filter(Cities.name.like('%'+term+'%')).limit(5)
+            query_data = session.query(Cities).filter(Cities.name.like("%"+term+"%")).limit(5)
             query_data_results = convert_to_array_of_dict(query_data)
             return jsonify(results=query_data_results) 
         except:
             return term
+
 @app.route('/')
 
 def index():
